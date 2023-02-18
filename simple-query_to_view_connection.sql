0e9d854f019ec96366681976350363452b5e6f06
vladi.student1@gmail.com <vladi.student1@gmail.com>
2023-02-18 01:00:19 +0000

    finished models

--- simple-query_to_view_connection.sql
+++ simple-query_to_view_connection.sql
@@ -1,1 +1,5 @@
-select * from `dbt-tutorial.jaffle_shop.customers`
+# select * from `dbt-tutorial.jaffle_shop.customers` limit 5
+
+#select * from `dbt-tutorial.jaffle_shop.orders` limit 5
+
+select * from `dbt-tutorial.stripe.payment` limit 5
