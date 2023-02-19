611b8c8a92c0ad50b34082b93a5b73561c4cac1f
vladi.student1@gmail.com <vladi.student1@gmail.com>
2023-02-19 17:24:12 +0000

    added analyses and seeds to project

---
+++ analyses/total_revenue.sql
@@ -0,0 +1,14 @@
+with
+    payments as (select * from {{ ref("stg_payments") }}),
+
+    aggregated as (
+
+
+        select sum(amount) as success_payment_total
+        from payments
+        where status = 'success'
+
+    )
+
+select *
+from aggregated
