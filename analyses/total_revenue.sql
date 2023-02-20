0ba0571868fc55faa584c382bd4fdb4649b46d85
vladi.student1@gmail.com <vladi.student1@gmail.com>
2023-02-20 01:27:03 +0000

    building int_orders_pivoted.sql(jinja lesson) on stg_payments model

--- analyses/total_revenue.sql
+++ analyses/total_revenue.sql
@@ -2,12 +2,9 @@ with
     payments as (select * from {{ ref("stg_payments") }}),
 
     aggregated as (
-
-
         select sum(amount) as success_payment_total
         from payments
         where status = 'success'
-
     )
 
 select *
