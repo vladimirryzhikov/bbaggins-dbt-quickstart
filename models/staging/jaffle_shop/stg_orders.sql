2299acbaf09c54819dc5d8865744dcf8e2dd2b77
vladi.student1@gmail.com <vladi.student1@gmail.com>
2023-02-20 23:01:28 +0000

    worked with macro limit_data_in_dev added timedate funcionality with BQ specall functions date_sub, date, current_datetime

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,2 +1,3 @@
 select id as order_id, user_id as customer_id, order_date, status
-from {{ source('jaffle_shop', 'orders')}}
+from {{ source("jaffle_shop", "orders") }}
+ {{ limit_data_in_dev("order_date",10000) }}
